521d771c3e04e1102da2f26a4cefc3652bd0c1fe
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-20 16:44:57 -0700

    statically designed my HTML and CSS

---
+++ data/todos.js
@@ -0,0 +1,24 @@
+const todos = [
+    {
+        task: 'Wash dog',
+        completed: true
+    },
+    {
+        task: 'Clean house',
+        completed: false
+    },
+    {
+        task: 'Do laundry',
+        completed: false
+    },
+    {
+        task: 'Build personal website',
+        completed: false
+    },
+    {
+        task: 'Embrioder mask',
+        completed: false
+    }
+];
+
+export default todos;
