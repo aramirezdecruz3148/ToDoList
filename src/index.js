d17d8da907bdff90e657a9b8038ae2ef202513b8
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-20 17:37:42 -0700

    added the stretch goal of templateHeader function and plugged it in on my index.js

---
+++ src/index.js
@@ -0,0 +1,27 @@
+//import array data
+import todos from '../data/todos.js';
+//import template functions
+import template from './todo-template.js';
+import templateHeader from './template-header.js';
+//import htmlToDOM function
+import htmlToDOM from '../src/html-to-dom.js';
+
+//reference the target parent elements
+const section = document.getElementById('todos');
+const header = document.getElementById('header');
+
+//call .forEach on the array
+todos.forEach(todo => {
+    //Convert todo object to html using template
+    const html = template(todo);
+    //Convert html to DOM using htmlToDOM
+    const dom = htmlToDOM(html);
+    //Append to list parent (<section>)
+    section.appendChild(dom);
+
+});
+
+//dynamically creating header
+const htmlHeader = templateHeader();
+const domHeader = htmlToDOM(htmlHeader);
+header.appendChild(domHeader);
---
+++ src/template-header.js
@@ -0,0 +1,9 @@
+function templateHeader() {
+    return /*html*/`
+    <h1>
+    To Do List
+    </h1>
+    `;
+}
+
+export default templateHeader;
